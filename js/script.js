3832e64d3c8c6dc57b260c04bc4718de46c95a25
Martin Sookael <martinsookael@gmail.com>
2013-05-27 19:53:12 +0200

    added log-in screen

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
 // JavaScript Document
 
 $(document).ready(function() {
-	$("#input").focus();
+
+	if ($("#username").is(":visible")) {
+		$("#username").focus();			
+	}
+	else {
+		$("#input").focus();			
+	}
+	
+	$('#login').ajaxForm(function() { 
+
+		var username = $('#username');
+		username = username.val();
+		name = String(username);
+				
+		if (username) { 
+			$('.message').show();
+			$("#input").focus();	
+			$('#message1').hide();
+		}
+		//console.log(username.val());
+
+	}); 
 
 	$('#send').ajaxForm(function() { 
-	  var input = $('#input');
+	  
+		var input = $('#input');
 				
 		switch(input.val()) {
 			case '.w':
-			$("#jetzt").before('<div class="message announce"><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">Online kasutajad</p><p>Vassili Koslakov, Silver Meikar, Marko Ausma, Sven Tiruson, Martin Sookael</p></div>');
+			$("#jetzt").before('<div class="message announce"><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">Online kasutajad</p><p>Vassili Koslakov, Silver Meikar, Marko Ausma, Sven Tiruson, Martin Sookael, Bender, '+name+'</p></div>');
 			break;
-
-			case '.who':
-			$("#jetzt").before('<div class="message announce"><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">Online kasutajad</p><p>Vassili Koslakov, Silver Meikar, Marko Ausma, Sven Tiruson, Martin Sookael</p></div>');
-			break;
-
+			
 			case '.h':
 			$("#jetzt").before('<div class="message announce"><div id="time">'+jQuery.timeago(new Date())+'</div><p><strong>.w</strong> - who - kes on kohal<br><strong>.h</strong> - help - näita seda ekraani siin <br>');
 			break;
 
 			case '.c':
-				$("#jetzt").before('<div class="message center"><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">User Name <img src="img/che.png" /></p></div>');
+				$("#jetzt").before('<div class="message center"><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">'+name+' <img src="img/che.png" /></p></div>');
 			break;
 
 			case '':
@@ -28,7 +46,7 @@ $(document).ready(function() {
 			break;
 			
 			default: 
-			$("#jetzt").before('<div class="message"><img src="img/ma.jpg" id="avatar" /><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">Marko Ausma</p><p>'+input.val()+'</p></div>')
+			$("#jetzt").before('<div class="message"><img src="img/ma.jpg" id="avatar" /><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">'+name+'</p><p>'+input.val()+'</p></div>')
 			break;
 		}
 		
