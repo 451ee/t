a96081cb73fb1e2f4fbf318d40ccdef7d9247d8d
Martin Sookael <martinsookael@gmail.com>
2013-05-27 14:37:25 +0200

    test

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 	$('#send').ajaxForm(function() { 
 	  var input = $('#input');
-		$("#jetzt").before('<div class="message"><img src="img/ms.jpg" id="avatar" /><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">Martin Sookael</p><p>'+input.val()+'</p></div>')
+		$("#jetzt").before('<div class="message"><img src="img/ma.jpg" id="avatar" /><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">Marko Ausma</p><p>'+input.val()+'</p></div>')
 		
 		input.val('');
 $(window).scrollTop($(document).height());
