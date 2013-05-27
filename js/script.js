e30ec5f71197fb9a78c6d62f00875f327da53337
Martin Sookael <martinsookael@gmail.com>
2013-05-27 19:14:58 +0200

    delete .e action

--- js/script.js
+++ js/script.js
@@ -5,18 +5,7 @@ $(document).ready(function() {
 
 	$('#send').ajaxForm(function() { 
 	  var input = $('#input');
-		
-		/*
-		var emote = input.val().match(/.e/);
-		if (emote) { 
-			emote = input.val().substr(3);
-			if (emote.length > 0) {
-				$("#jetzt").before('<div class="message center"><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">User Name '+emote+'</p></div>');
-			} 
-			input.val('');
-		}
-		*/
-		
+				
 		switch(input.val()) {
 			case '.w':
 			$("#jetzt").before('<div class="message announce"><div id="time">'+jQuery.timeago(new Date())+'</div><p id="name">Online kasutajad</p><p>Vassili Koslakov, Silver Meikar, Marko Ausma, Sven Tiruson, Martin Sookael</p></div>');
@@ -27,7 +16,7 @@ $(document).ready(function() {
 			break;
 
 			case '.h':
-			$("#jetzt").before('<div class="message announce"><div id="time">'+jQuery.timeago(new Date())+'</div><p><strong>.w</strong> - who - kes on kohal<br><strong>.h</strong> - help - näita seda ekraani siin <br><!--<strong>.e</strong> - tegevus - proovi näiteks ".e tantsib"<br></p></div>-->');
+			$("#jetzt").before('<div class="message announce"><div id="time">'+jQuery.timeago(new Date())+'</div><p><strong>.w</strong> - who - kes on kohal<br><strong>.h</strong> - help - näita seda ekraani siin <br>');
 			break;
 
 			case '.c':
