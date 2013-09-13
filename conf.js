f5cfcc53d92ce882dce3db68aefcad3f35fc4006
Martin Sookael <martinsookael@gmail.com>
2013-09-13 19:33:12 +0300

    added conf file

---
+++ conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+    general: {
+        host: '127.0.0.1'
+      , port: 3001
+    },
+    mysql: {
+        host: '127.0.0.1'
+      , user: 'root'
+      , password: 'root'
+      , database: 'datadatadata'
+      , port: '8889'
+      , url: 'http://127.0.0.1:3000'
+    }
+};
