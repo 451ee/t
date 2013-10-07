850e3929b71194824f5ecc5cf9b5222260ada887
Martin Sookael <martinsookael@gmail.com>
2013-10-07 16:17:02 -0400

    added first memeing files

---
+++ public/javascripts/meme.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function() { 
+/*
+    var exp = /^meme/;
+    var match =  text.match(/^meme/);
+    
+    if(match !== null){ // is image
+        return text.replace(exp,"<a href='$1' target='_blank'><img class='full' src='$1' /></a>"); 
+    } */
+
+    
+/*    
+    // Memeing logic
+    var canvas  = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+    
+    
+    canvas.width  = 500;
+    canvas.height = 500;
+    canvas.style.borderRadius = "15px";
+    document.body.appendChild(canvas); // Add canvas to DOM
+    
+    
+    // GRAPHICS
+    var canvasImage = new Image();
+    //images/meme/1stworldproblem.png
+    //canvasImage.src = 'http://www.fabricspraypaint.com/images/T-Shirts/S1110.jpg';
+    canvasImage.src = 'http://127.0.0.1:3001/images/meme/1stworldproblem.png';
+    
+    
+    // GRAPHICS TO CANVAS ///////////////////
+    
+    function drawCanvas(){ 
+        ctx.drawImage(canvasImage, 0, 0);
+    }
+    function drawText( text ){
+        ctx.font = '20px Impact';
+        ctx.textAlign = 'center';
+        ctx.fillStyle = 'rgba(70, 70, 255, 0.7)';
+        ctx.fillText(text, canvas.width/2, canvas.height/2);
+    }
+    
+    drawCanvas();
+    drawText('Liiga hilja hommikusöögiks');
+*/    
+});
