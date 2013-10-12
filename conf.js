f79a751a3caa5a0b6d0f5bffd30c8a640b5c07f1
Martin Sookael <martinsookael@gmail.com>
2013-10-12 02:45:55 -0400

    readme

--- conf.js
+++ conf.js
@@ -1,12 +1,12 @@
 module.exports = {
     general: {
       //  host: '192.168.43.142'
-        host: '127.0.0.1'
+        host: '127.0.0.1' // notice the missing "http://"
       , port: 3001
       , url: 'http://127.0.0.1:3001'
     }
   , db: {
-        usesDb: true // true - uses db, false - uses not.
+        usesDb: true // true - uses db, false - uses not. // IF TALKER WON'T START, START DEBUGGING FROM HERE
       , dbName: 'talker'
       , collectionName: 'chat'
     }
