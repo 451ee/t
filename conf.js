49dd0a5cd91e813d58131a4018f4f3465fa665ef
Martin Sookael <martinsookael@gmail.com>
2013-10-17 18:41:01 -0400

    use no db default

--- conf.js
+++ conf.js
@@ -1,6 +1,5 @@
 module.exports = {
     general: {
-      //  host: '192.168.43.142'
         host: '127.0.0.1' // notice the missing "http://"
       , port: 3001
       , url: 'http://127.0.0.1:3001'
@@ -8,7 +7,7 @@ module.exports = {
         // annonAccess IS NOT WORKING 
     }
   , db: {
-        usesDb: true // true - uses db, false - uses not. // IF TALKER WON'T START, START DEBUGGING FROM HERE
+        usesDb: false // true - uses db, false - uses not. // IF TALKER WON'T START, START DEBUGGING FROM HERE
       , dbName: 'talker'
       , collectionName: 'chat'
     }
